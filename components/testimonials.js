76cf65e678bf00bb00ac202115fb367b8ac5ae01
davincidreams <lisamegawatts@gmail.com>
2023-07-10 05:27:19 -0400

    testimonials

--- components/testimonials.js
+++ components/testimonials.js
@@ -13,42 +13,40 @@ const Testimonials  = () => {
         <div className="lg:col-span-2 xl:col-auto">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              Share a real <Mark>testimonial</Mark>
-              that hits some of your benefits from one of your popular customer.
+              Simplifying grant applications is a <Mark>gamechanger</Mark> for organizational efficiency. Grant Bot is like turbo tax for grants.
             </p>
 
             <Avatar
               image={userOneImg}
-              name="Sarah Steiner"
-              title="VP Sales at Google"
+              name="John Doe"
+              title="Experienced Grant Writer"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              Make sure you only pick the <Mark>right sentence</Mark>
-              to keep it short and simple.
+              Grant Bot's proprietary algorithms <Mark>simplify </Mark>
+              the grant colletion process.
             </p>
 
             <Avatar
               image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
+              name="John Doe"
+              title="Machine Learning Expert"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve seen. I
-              would use this for anything.
+              Grant Bot is an <Mark>awesome</Mark> idea for leveraging AI to better serve people.
             </p>
 
             <Avatar
               image={userThreeImg}
-              name="Gabrielle Winn"
-              title="Co-founder of Acme Inc"
+              name="Jane Doe"
+              title="Tech Co-founder"
             />
           </div>
         </div>
