049ae717235fe695d0424f9813d8420e36b128fb
davincidreams <lisamegawatts@gmail.com>
2023-07-10 03:51:00 -0400

    hero

--- components/hero.js
+++ components/hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
             Introducing Grant Bot: Your Ultimate Guide to Grant Applications
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-            Unlock Funding Opportunities with Ease and Confidence
+            Your Ultimate Guide to Grant Applications
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
